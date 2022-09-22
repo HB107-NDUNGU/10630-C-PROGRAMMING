930ed424ab46e7401964535f1192d09586547075
HB107-NDUNGU <113588443+HB107-NDUNGU@users.noreply.github.com>
2022-09-22 13:02:55 +0300

    Add files via upload

---
+++ voters eligibility.cpp
@@ -0,0 +1,36 @@
+//program to check voter eligibility
+
+#include <iostream>
+
+
+using namespace std;
+
+int main() 
+
+{
+
+    int age;
+
+    cout<<"Enter your age"<<endl;
+
+    cin>>age;
+
+    
+    if(age>=18)
+
+    {
+        cout<<"You are eligible to vote"<<endl;
+
+    }
+
+    else
+
+    {
+
+    cout<<"Not eligible";
+
+    }
+
+    return 0;
+
+}
